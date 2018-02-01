f23863a3fe2c4dcff25420f500b2422f0cff9d02
nfritzen <mail@nikolas-fritzen.de>
2018-02-01 14:12:53 +0100

    Test

--- menue.java
+++ menue.java
@@ -53,7 +53,7 @@ public class menue{
                                 System.out.println("BaerMitUmlaut");
                                 break;
                             default:
-                                    System.out.println("Fehler: Kenne ich nicht!");
+                                    System.out.println("Fehlär: Kenne ich nicht!");
                             }
                     }
             }
