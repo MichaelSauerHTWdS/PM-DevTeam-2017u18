6481e45c1140651c42f7f9a29b6f47eed97dc756
TheGreenkey <thegreenkeyy@gmail.com>
2018-02-01 14:47:04 +0100

    THIS IS JAVA!

--- output.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, char **argv)
-{
-	std::cout << "Java ist nur fuer die coolen Kidz" << std::endl;
-	std::cout << "I am the MasterOFMasters" << std::endl;
-	return 0;
-}
